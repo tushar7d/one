759449d738e6f56e76b3bde7a7545a4f9f7901ea
Tushar <tdebnath@expedia.com>
2019-10-24 13:42:32 +0530

    Revert "changes"
    This reverts commit b271bc3b6a9098d7f631eafea22b39976a04b9b9.

--- src/Cards/DynamicSection.js
+++ src/Cards/DynamicSection.js
@@ -58,48 +58,6 @@ let increment = (price, deal, pp, cc, mm) => {
 
   return updatedPrice;
 };
-let slasherprice = (price, deal, pp, cc, mm) => {
-  let updatedPrice = price;
-
-  //Mip
-  if (deal === "0") {
-    updatedPrice = updatedPrice + (5 - parseInt(deal, 10));
-  }
- //Mod
-  if (deal === "1") {
-    updatedPrice = updatedPrice + (5 - parseInt(deal, 10));
-  }
-  //Mobile exclusive
-  if (deal === "2") {
-    updatedPrice = updatedPrice + (5 - parseInt(deal, 10));
-  }
-  //normal offer
-  if (deal === "3") {
-    updatedPrice = updatedPrice + (5 - parseInt(deal, 10));
-  }
-  //no offer
-  if (deal === null) {
-    updatedPrice = updatedPrice - 0;
-  }
-  //pay later
-  if (pp) {
-    updatedPrice = updatedPrice - 1;
-  }
-  //free cancellation
-  if (cc) {
-    updatedPrice = updatedPrice -1;
-  }
- //mileage
-  if (mm === "") {
-    updatedPrice = updatedPrice - 2;
-  }
-  //mileage
-  if (mm === "400") {
-    updatedPrice = updatedPrice - 1;
-  }
-
-  return updatedPrice;
-};
 
 let DynamicSection = props => {
   const base_d = parseInt(props.offer.daily, 10);
@@ -113,10 +71,6 @@ let DynamicSection = props => {
     props.cc,
     props.mileage
   );
-  let slasher = slasherprice(base_t,props.deal,
-    props.paylater,
-    props.cc,
-    props.mileage);
   let total = daily * 4 + tax;
 
   return (
@@ -183,7 +137,7 @@ let DynamicSection = props => {
       </Stack>
 
       <Stack horizontal split top width="100%">
-        <Stack vertical mt="2" width="calc(60% - 12px )" pl="3">
+        <Stack vertical mt="2" width="calc(70% - 12px )" pl="3">
           {props.mileage === "" ? (
             <Text type300 normal color={"neutral__700"}>
               Unlimited mileage
@@ -215,7 +169,7 @@ let DynamicSection = props => {
             </Text>
           )}
         </Stack>
-        <Stack horizontal right mid width="calc(38% - 12px )" pr="1">
+        <Stack horizontal right mid width="calc(28% - 12px )" pr="1">
           <Stack vertical right top width="100%" mt="2" pr="1">
             <Text type600 bold color={"neutral__800"} textAlign="right">
               ${daily}
@@ -224,7 +178,7 @@ let DynamicSection = props => {
               per day
             </Text>
             <Text type200 normal color={"neutral__500"} textAlign="right">
-              $<Text.span type200 normal color={"neutral__500"} style={{textDecoration: "line-through"}}>{slasher}</Text.span> ${total} total
+              ${total} total
             </Text>
           </Stack>
           <Icon value={{ color: "#005aeb", size: "36px" }}>
